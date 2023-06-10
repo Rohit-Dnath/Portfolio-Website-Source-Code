02a0729531bef0fe07b40ff579fdebb13e0ef711
Rohit Debnath <136178612+Rohit-Dnath@users.noreply.github.com>
2023-06-10 20:27:02 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# portfolio
+
+Here is My portfolio Website-
+
+Tech stack used -
+1) HTML
+2) JavaScript
+3) Css
+
+For More Info-
+Contact- instagram.com/r0dth
---
+++ assets/js/main.js
@@ -0,0 +1,129 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+  let current = '';
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop - 390) {
+      current = section.getAttribute('id');
+    }
+  })
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.classList.contains(current)) {
+      link.classList.add('active');
+    }
+  })
+})
+
+// function linkAction() {
+//   /*Active link*/
+//   navLinks.forEach((n) => n.classList.remove("active"));
+//   this.classList.add("active");
+
+//   /*Remove menu mobile*/
+const navMenu = document.getElementById("nav-menu");
+//   navMenu.classList.remove("show");
+// }
+navLinks.forEach((n) => n.addEventListener("click", () => { navMenu.classList.remove("show") }));
+
+/*===== COPY Email =====*/
+const copy = document.getElementById("copy");
+copy.addEventListener("click", () => {
+  navigator.clipboard.writeText("debnathrohit97@gmail.com");
+  copy.innerHTML = "copied";
+  setTimeout(() => {
+    copy.innerHTML = null;
+  }, 1000);
+});
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+/*SCROLL HOME*/
+sr.reveal(".home-title", {});
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".home-img", { delay: 400 });
+sr.reveal(".home-social-icon", { interval: 200 });
+
+/*SCROLL ABOUT*/
+sr.reveal(".about-img", {});
+sr.reveal(".about-subtitle", { delay: 400 });
+sr.reveal(".about-text", { delay: 400 });
+
+/*SCROLL SKILLS*/
+sr.reveal(".skills-subtitle", {});
+sr.reveal(".skills-text", {});
+sr.reveal(".skills-data", { interval: 100 });
+// sr.reveal(".skills-img", { delay: 600 });
+
+/*SCROLL projects*/
+sr.reveal(".project-img", { interval: 200 });
+
+/*SCROLL CONTACT*/
+  // sr.reveal(".contact-input", { interval: 200 });
+
+  function myFunction(){
+    var element = document.body;
+    element.classList.toggle("dark-mode")
+  }
+
+
+  var messageArr = ["Full Stack Developer :)", "Frontend Developer", "Backend Developer", "Mern Developer"];
+  var textPosition = 0;
+  var speed = 200;
+
+  typewriter = () => {
+    // for(let i = 0; i < messageArr.length; i++) {
+    document.querySelector("#jobTitle").innerHTML = messageArr[0].substring(0, textPosition)  ;
+    if(textPosition ++  != messageArr[0].length)
+        setTimeout(typewriter, speed)
+  }
+
+
+  window.addEventListener("load" , typewriter);
+
+
+
+
+//Slider comp strts
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+//slider comp ends
+
